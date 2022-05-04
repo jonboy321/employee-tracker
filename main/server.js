635a0f7ee0352566cbd07489d5081ac60171688f
Jonny Gemme <gemmejonathan@gmail.com>
2022-05-04 01:24:28 -0400

    getting js file to pull from db using queries, going to bed

--- main/server.js
+++ main/server.js
@@ -4,13 +4,30 @@ const fs = require('fs');
 
 // Set up connection to database
 const db = mysql.createConnection(
-    {
-      host: 'localhost',
-      // MySQL username,
-      user: 'root',
-      // MySQL password
-      password: 'Hazelbasil96!',
-      database: 'employees_db'
-    },
-    console.log(`Connected to the employees_db database.`)
-  );
+  {
+    host: 'localhost',
+    // MySQL username,
+    user: 'root',
+    // MySQL password
+    password: 'Hazelbasil96!',
+    database: 'employees_db'
+  },
+  console.log(`Connected to the employees_db database.`)
+);
+
+// Query database
+db.query('SELECT department_name, id FROM departments', function (err, results) {
+  console.table(results);
+});
+
+const employees = [];
+
+const createEmployee = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            list: 'role',
+            message: "What is the employee's role?",
+        }
+    ]);
+};
